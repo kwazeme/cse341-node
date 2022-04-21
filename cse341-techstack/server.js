76810e096b4f4585b57377b71c17e13f02ddac82
KWAZEME OGUBIE <ogu21006@byui.edu>
2022-04-22 00:29:19 +0200

    tech stack webserver

---
+++ cse341-techstack/server.js
@@ -0,0 +1,11 @@
+ const express = require('express');
+ const app = express();
+ const port = process.env.PORT || 3000;
+
+ app.get('/', (req, res) => {
+     res.send("Clara Ogubie")
+ })
+
+ app.listen(port, () => {
+     console.log(`Runnning on port ${port}`)
+ })
