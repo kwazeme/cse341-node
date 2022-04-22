6f811c878fa5ff33fab69a5ec014da8e415025d1
KWAZEME OGUBIE <ogu21006@byui.edu>
2022-04-22 21:51:42 +0200

    remove spaces in web:node

--- cse341-techstack/server.js
+++ cse341-techstack/server.js
@@ -1,10 +1,10 @@
  const express = require('express');
  const app = express();
- const port = process.env.PORT || 5000;
+ const port = process.env.PORT||5000;
 
  app.get('/', (req, res) => {
      res.send("Clara Ogubie")
- })
+ });
 
  app.listen(port, () => {
      console.log(`Runnning on port ${port}`)
