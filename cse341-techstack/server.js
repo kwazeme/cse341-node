1be419db9dca8d7849d09fca77159dc59907dc04
KWAZEME OGUBIE <ogu21006@byui.edu>
2022-04-22 21:06:55 +0200

    Add env.Port || 5000 back

--- cse341-techstack/server.js
+++ cse341-techstack/server.js
@@ -1,6 +1,6 @@
  const express = require('express');
  const app = express();
- const port = process.env.PORT;
+ const port = process.env.PORT || 5000;
 
  app.get('/', (req, res) => {
      res.send("Clara Ogubie")
